89883f31af1be55d916ede2eb76f5911cabed35c
narae <narae_j@sandoll.co.kr>
2022-10-04 11:45:00 +0900

    퍼블용과 스킨 적용용 파일 구분

---
+++ tistory_skin/HBD_daniel_pub/images/common.js
@@ -0,0 +1,28 @@
+var agent = navigator.userAgent.toLowerCase();
+
+if( navigator.appName == 'Netscape' && navigator.userAgent.search('Trident') != -1 || (agent.indexOf("msie") != -1)) {
+  alert("이 스킨은 인터넷익스플로러를 지원하지 않습니다")
+}
+
+function openNav() {
+  document.getElementById("mySidenav").classList.add("active");
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").classList.remove("active");
+}
+
+$(function(){
+  var swiper = new Swiper(".mainBanner", {
+   
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+  });
+})
