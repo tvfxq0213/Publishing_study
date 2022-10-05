e535aec293d99b764cc1636e02167a2abda5d25f
narae <narae_j@sandoll.co.kr>
2022-10-05 14:03:28 +0900

    수정

--- tistory_skin/HBD_daniel_pub/images/common.js
+++ tistory_skin/HBD_daniel_pub/images/common.js
@@ -1,4 +1,6 @@
 var agent = navigator.userAgent.toLowerCase();
+var backgroundColor = ['#FFB3B3', '#FFDBA4', '#FFE9AE', '#C1EFFF', '#B1B2FF','#AAC4FF', '#F5F0BB', '#C4DFAA', '#90C8AC', '#73A9AD' , '#F3C5C5', '#FFB6B6','#DFBAF7','#ECFFC1'];
+
 
 if( navigator.appName == 'Netscape' && navigator.userAgent.search('Trident') != -1 || (agent.indexOf("msie") != -1)) {
   alert("이 스킨은 인터넷익스플로러를 지원하지 않습니다")
@@ -10,6 +12,11 @@ function openNav() {
 
 function closeNav() {
   document.getElementById("mySidenav").classList.remove("active");
+}
+
+function randomBackgroundBG(element){
+	var num = Math.floor(Math.random() * backgroundColor.length); 
+  $(element).css('background-color',backgroundColor[num]);
 }
 
 $(function(){
